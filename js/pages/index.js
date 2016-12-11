363d8fba5500bf9eb1669f237227bd0f3542c003
zhaotao <zhaotao_dyx@163.com>
2016-12-12 00:44:50 +0800

    添加查询框

--- js/pages/index.js
+++ js/pages/index.js
@@ -2,6 +2,7 @@
  * Created by zhao on 2016/12/10.
  */
 
+//地图初始化
 function pageLoadMap(mapId,mapUrl,centerPoint,myStartZoom,supportName){
     esriTileLayerXian80.setMapUrl(mapUrl);
     var mapOptions=esriTileLayerXian80.getMapOptions();
@@ -15,5 +16,16 @@ function pageLoadMap(mapId,mapUrl,centerPoint,myStartZoom,supportName){
         attribution: supportName,
     });
     map.addLayer(tileLayer).setView(centerPoint,myStartZoom);
-    L.control.scale().setPosition('bottomleft').addTo(map);
+    map.zoomControl.setPosition("topright");
+
+    //todo  not work,need extend crs's distance funtion
+    // L.control.scale().setPosition('bottomleft').addTo(map);
+}
+//查询插件初始化
+function searchPanelInit() {
+    var searchText = [
+        "您想知道什么信息?",
+        "检索四规水务信息从这里开始",
+    ];
+    $('#search').placeholderTypewriter({text: searchText});
 }
