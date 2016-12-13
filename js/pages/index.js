8112d75fe0788c877c98afa2d7c498bf0b0d3fa1
zhaotao <zhaotao_dyx@163.com>
2016-12-13 22:34:53 +0800

    添加图层组件

--- js/pages/index.js
+++ js/pages/index.js
@@ -2,13 +2,14 @@
  * Created by zhao on 2016/12/10.
  */
 
+var map=null;
 //地图初始化
 function pageLoadMap(mapId,mapUrl,centerPoint,myStartZoom,supportName){
     esriTileLayerXian80.setMapUrl(mapUrl);
     var mapOptions=esriTileLayerXian80.getMapOptions();
     var myMaxZoom=esriTileLayerXian80.getMaxZoom();
     var myMinZoom=esriTileLayerXian80.getMinZoom();
-    var map = L.map(mapId, mapOptions);
+    map = L.map(mapId, mapOptions);
     var tileLayer = new L.esri.tiledMapLayer({
         url: mapUrl,
         maxZoom: myMaxZoom,
@@ -32,22 +33,76 @@ function searchPanelInit() {
 //图层组件初始化
 function layerPanelInit() {
     $('#layerPanel').popmenu({
-        'width': '220px',
+        'width': '240px',
         'background':'#FFFFFF',
         'focusColor':'#B2E7FF',
         'borderRadius':'5px',
         'top': '0',
-        'left': '190',
+        'left': '200',
         'border':'2px solid #4280ed'
     });
     $('#layerContainer').jstree({'plugins':["wholerow","checkbox"],
         'core' : {
-            'data' : [
-                { "text" : "水务专题服务", "children" : [
-                    { "text" : "水源保护区" },
-                    { "text" : "水源取水口" },
-                ]}
-            ]
+            'data' :layerData,
+        }
+    });
+    $('#layerContainer').on("changed.jstree", function (e, data) {
+        if (data.node!=null){
+           var actid=data.action;
+           var layerId=data.node.id;
+           var layerAdd=data.node.data;
+           if (actid=="deselect_node"){
+               removeMapLayer(layerId,layerAdd);
+           }else if (actid=="select_node"){
+
+               addMapLayer(layerId,layerAdd);
+           }
         }
     });
 }
+
+function removeMapLayer(layerId,layerAdd) {
+    var restlayer=getEsriRestLayer(layerAdd);
+    map.remove(restlayer);
+}
+
+function addMapLayer(layerId,layerAdd) {
+    var restlayer=getEsriRestLayer(layerAdd);
+    debugger;
+    map.addLayer(restlayer);
+}
+function getEsriRestLayer(layerAdd) {
+    var tid=layerAdd.split('#')[1];
+    var tadd=layerAdd.split('#')[0];
+    var myMaxZoom=esriTileLayerXian80.getMaxZoom();
+    var myMinZoom=esriTileLayerXian80.getMinZoom();
+    var restlayer = new L.esri.tiledMapLayer({
+        url: tadd,
+        maxZoom: myMaxZoom,
+        minZoom: myMinZoom,
+        attribution: supportName,
+    });
+    return restlayer;
+}
+
+//all temp date
+var layerData=[
+    { 'text' : '服务目录', 'children' : [
+        { 'id' :'id1',
+            'text' : '基础服务',
+            'children':[{
+                'id' :'id11',
+                'text' : '惠城区行政区划',
+                'data' : 'http://106.39.231.23/ArcGIS/rest/services/HZDG/%E6%83%A0%E5%9F%8E%E5%8C%BA%E8%A1%8C%E6%94%BF%E5%8C%BA%E5%88%92/MapServer#0'
+            }],
+        },
+        { 'id' :'id2',
+            'text' : '水务专题服务',
+            'children':[{
+                'id' :'id21',
+                'text' : '水源保护区',
+                'data' : 'http://106.39.231.23/ArcGIS/rest/services/HZDG/%E6%B0%B4%E6%BA%90%E4%BF%9D%E6%8A%A4%E5%8C%BA/MapServer#1'
+            }],
+        },
+    ]}
+];
